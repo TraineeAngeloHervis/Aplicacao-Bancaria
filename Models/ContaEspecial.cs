3fcb9efaa0740669957175a5a14191b4587e957b
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-06-24 10:58:18 -0300

    projeto feito, falta os testes

---
+++ Models/ContaEspecial.cs
@@ -0,0 +1,36 @@
+﻿namespace Aplicacao_Bancaria.Models;
+
+public class ContaEspecial : ContaBancaria
+{
+    private double Limite { get; set; }
+    
+    public ContaEspecial (int numeroConta, double limite)
+    {
+        NumeroConta = numeroConta;
+        Limite = limite;
+    }
+    
+    public override void Sacar(double valor)
+    {
+        if (Saldo + Limite >= valor)
+        {
+            Saldo -= valor;
+            Console.WriteLine($"Saque de R$ {valor} realizado com sucesso!");
+        }
+        if (Saldo + Limite < valor)
+        {
+            Console.WriteLine("Saque não realizado! Saldo insuficiente!");
+        }
+    }
+    
+    public override void Depositar(double valor)
+    {
+        Saldo += valor;
+        Console.WriteLine($"Depósito de R$ {valor} realizado com sucesso!");
+    }
+    
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Conta Especial\nNúmero da Conta: {NumeroConta}\nSaldo: R$ {Saldo}\nLimite: R$ {Limite}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Aplicacao_Bancaria.Models;
+
+var contaCorrente1 = new ContaCorrente(1, 5);
+var contaCorrente2 = new ContaCorrente(2, 5);
+var contaEspecial1 = new ContaEspecial(2, 1000);
+var contaEspecial2 = new ContaEspecial(2, 2000);
+
+contaCorrente1.Depositar(100);
+contaCorrente1.Sacar(50);
+contaCorrente1.MostrarDados();
+
+contaCorrente2.Depositar(200);
+contaCorrente2.Sacar(100);
+contaCorrente2.MostrarDados();
+
+contaEspecial1.Depositar(300);
+contaEspecial1.Sacar(200);
+contaEspecial1.MostrarDados();
+
+contaEspecial2.Depositar(400);
+contaEspecial2.Sacar(300);
+contaEspecial2.MostrarDados();
+
+contaEspecial1.Sacar(2000);
+contaEspecial1.MostrarDados();
